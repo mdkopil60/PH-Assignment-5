3250c698b9437b84ec15841b62ad62a0af8292eb
KOPIL UDDIN <kopil7132@gmail.com>
2026-03-08 15:57:59 +0600

    displayIssuees Function done

--- script.js
+++ script.js
@@ -15,49 +15,84 @@ async function loadIssues(type) {
 
 // Display Issues Card
 function displayIssues(issues) {
+
     const container = document.getElementById("issuesContainer")
     container.innerHTML = ""
+
     document.getElementById("issueCount").innerText = issues.length
+
     issues.forEach(issue => {
 
         const borderColor = issue.status === "open"
             ? "border-green-500"
             : "border-purple-500"
+
+        const statusIcon = issue.status === "open"
+            ? "./assets/Open-Status.png"
+            : "./assets/Closed- Status .png"
+
+        const priorityColor =
+            issue.priority === "HIGH"
+                ? "bg-red-100 text-red-600"
+                : issue.priority === "MEDIUM"
+                    ? "bg-yellow-100 text-yellow-600"
+                    : "bg-gray-100 text-gray-600"
+
         container.innerHTML += `
-    <div onclick="showModal(${issue.id})"
-    class="bg-white rounded-lg shadow-md border-t-4 ${borderColor} p-4 cursor-pointer hover:shadow-lg transition">
-    <div class="flex justify-between items-center mb-2">
-    <span class="text-xs bg-red-100 text-red-600 px-2 py-1 rounded">
-    ${issue.priority}
-    </span>
-    </div>
-    <h2 class="font-semibold text-sm mb-1">
-    ${issue.title}
-    </h2>
-    <p class="text-xs text-gray-500 mb-3">
-    ${issue.description}
-    </p>
-    <div class="flex gap-2 mb-3">
 
-    <span class="text-xs bg-red-100 text-red-600 px-2 py-1 rounded">
-    ${issue.category}
-    </span>
+        <div onclick="showModal(${issue.id})"
+        class="bg-white rounded-xl shadow-md border-t-4 ${borderColor} p-4 cursor-pointer hover:shadow-lg transition duration-300">
 
-    <span class="text-xs bg-yellow-100 text-yellow-600 px-2 py-1 rounded">
-    ${issue.labels}
-    </span>
+            <!-- Top Section -->
+            <div class="flex justify-between items-center mb-2">
 
-    </div>
+                <!-- Left Icon -->
+                <img src="${statusIcon}" class="w-5 h-5">
 
-    <div class="text-xs text-gray-400">
-    #${issue.id} by ${issue.author}  
-    <br>
-    ${issue.createdAt}
-    </div>
-    </div>
-    `
+                <!-- Priority -->
+                <span class="text-xs px-2 py-1 rounded-full ${priorityColor}">
+                    ${issue.priority}
+                </span>
+
+            </div>
+
+            <!-- Title -->
+            <h2 class="font-semibold text-sm mb-1">
+                ${issue.title}
+            </h2>
+
+            <!-- Description -->
+            <p class="text-xs text-gray-500 mb-3 line-clamp-2">
+                ${issue.description}
+            </p>
+
+            <!-- Labels -->
+            <div class="flex gap-2 mb-3">
+
+                <span class="text-xs bg-red-100 text-red-600 px-2 py-1 rounded-full">
+                    ${issue.category}
+                </span>
+
+                <span class="text-xs bg-yellow-100 text-yellow-600 px-2 py-1 rounded-full">
+                    ${issue.label}
+                </span>
+
+            </div>
+
+            <!-- Footer -->
+            <div class="text-xs text-gray-400">
+                #${issue.id} by ${issue.author}
+                <br>
+                ${issue.createdAt}
+            </div>
+
+        </div>
+        `
     })
 }
+
+
+
 // Modal (Issue Details)
 async function showModal(id) {
     const res = await fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`)
